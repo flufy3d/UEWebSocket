18fc801d69cb33c429996ec3acc54fa79908b849
flufy3d <flufy3d@gmail.com>
2016-11-28 18:56:31 +0800

    add tick funtion

--- Source/UEWebSocket/Private/UEWebSocketInst.cpp
+++ Source/UEWebSocket/Private/UEWebSocketInst.cpp
@@ -7,7 +7,6 @@ UUEWebSocketInst::UUEWebSocketInst(const class FObjectInitializer& PCIP)
 {
 
 
-
 }
 
 void UUEWebSocketInst::Init(UObject* WorldContextObject)
@@ -16,8 +15,40 @@ void UUEWebSocketInst::Init(UObject* WorldContextObject)
 }
 
 
-
 void UUEWebSocketInst::Send(const FString& data)
 {
+	ReceiveEventEmitter.Broadcast(data);
+}
 
+void UUEWebSocketInst::Tick(float DeltaTime)
+{
+	static float tmp = 0;
+	tmp += DeltaTime;
+	GEngine->AddOnScreenDebugMessage(0, 0, FColor::Green, FString::SanitizeFloat(tmp));
+
+}
+
+bool UUEWebSocketInst::IsTickable() const
+{
+	return true;
+}
+
+bool UUEWebSocketInst::IsTickableInEditor() const
+{
+	return false;
+}
+
+bool UUEWebSocketInst::IsTickableWhenPaused() const
+{
+	return false;
+}
+
+TStatId UUEWebSocketInst::GetStatId() const
+{
+	return TStatId();
+}
+
+UWorld* UUEWebSocketInst::GetWorld() const
+{
+	return GetOuter()->GetWorld();
 }
--- Source/UEWebSocket/Public/UEWebSocketInst.h
+++ Source/UEWebSocket/Public/UEWebSocketInst.h
@@ -1,9 +1,14 @@
 #pragma once
 #include "UEWebSocketInst.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FReceiveEvent, const FString&, data);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FConnectedEvent, const FString&, data);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FCloseEvent, const FString&, data);
+
+DECLARE_DELEGATE_OneParam(FReceiveCPPEvent,const FString&);
 
 UCLASS(ClassGroup = UEWebSocket, Blueprintable)
-class UUEWebSocketInst : public UObject
+class UEWEBSOCKET_API UUEWebSocketInst : public UObject , public FTickableGameObject
 {
 
     GENERATED_BODY()
@@ -20,6 +25,22 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "UEWebSocket")
 		void Send(const FString& data);
 
+	UPROPERTY(BlueprintAssignable)
+		FReceiveEvent ReceiveEventEmitter;
 
+	UPROPERTY(BlueprintAssignable)
+		FReceiveEvent FConnectedEvent;
+
+	UPROPERTY(BlueprintAssignable)
+		FReceiveEvent FCloseEvent;
+
+
+	void Tick(float DeltaTime) override;
+	bool IsTickable() const override;
+	bool IsTickableInEditor() const override;
+	bool IsTickableWhenPaused() const override;
+	TStatId GetStatId() const override;
+
+	UWorld* GetWorld() const override;
 
 };
